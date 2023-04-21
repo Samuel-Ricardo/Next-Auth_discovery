02cd4ffedc2fffed129cbbfc7a85910b99718151
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-21 11:34:38 -0300

    :sparkles: update: env mapping (@config/env)

--- next-auth-demo/src/config/env.ts
+++ next-auth-demo/src/config/env.ts
@@ -4,3 +4,11 @@ export const ENV_CHECK = process.env
 
 export const API_URL = process.env.API_URL
 export const AUTH_URL = `${process.env.API_URL}/api/auth`
+
+export const GITHUB = {
+  ID:process.env.GITHUB_ID,
+  SECRET:process.env.GITHUB_SECRET,
+}
+
+export const NEXT_AUTH= {SECRET:process.env.NEXT_AUTH_SECRET}
+
