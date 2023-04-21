f6fa0cceaa27212c21c64ea109453e661b9b8f5d
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-21 10:54:38 -0300

    :gear: create: config - env.ts (@config)

---
+++ next-auth-demo/src/config/env.ts
@@ -0,0 +1,6 @@
+console.log({ENV_CHECK: process.env})
+
+export const ENV_CHECK = process.env
+
+export const API_URL = process.env.API_URL
+export const AUTH_URL = `${process.env.API_URL}/api/auth`
