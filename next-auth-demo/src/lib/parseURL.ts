5a9d6265c66f9c3f2e14ce978fd5e73d3ed7c018
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-21 11:11:53 -0300

    :sparkles: create: lib - parseURL (@lib)

---
+++ next-auth-demo/src/lib/parseURL.ts
@@ -0,0 +1,29 @@
+import { AUTH_URL } from "@config/env";
+import { IInternalURL } from "@Types";
+import { URL } from "url";
+
+
+/** Returns an `URL` like object to make requests/redirects from server-side */
+export default function parseURL(url?:string):IInternalURL {
+
+  const defaultUrl = new URL(AUTH_URL)
+
+  if(url && !url.startsWith("http")) {
+    url = `https://${url}`;
+  }
+
+  const _url = new URL(url ?? defaultUrl);
+  const path = (_url.pathname === "/" ? defaultUrl.pathname : _url.pathname)
+                // Remove trailing slash
+                .replace(/\/$/, "")
+  
+  const base = `${_url.origin}${path}`;
+
+  return {
+    origin: _url.origin,
+    host: _url.host,
+    path,
+    base,
+    toString: () => base,
+  }
+} 
