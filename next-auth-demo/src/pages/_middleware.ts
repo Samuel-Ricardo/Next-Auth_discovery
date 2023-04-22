38e6283d32993701c3c95919a79cdb6554aafb36
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-22 09:25:05 -0300

    :sparkles: create: middleware - handle auth session (@pages)

---
+++ next-auth-demo/src/pages/_middleware.ts
@@ -0,0 +1,21 @@
+import { BASE_PATH, ERROR, SIGNIN } from "@/config/pages/routes";
+import { getSession } from "next-auth/react";
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function handle(req: NextRequest, res:NextResponse){
+  if (
+    req.nextUrl.pathname.startsWith(BASE_PATH) ||
+    [SIGNIN, ERROR].includes(req.nextUrl.pathname)
+  ) { return }
+
+  const sesssion = await getSession(
+      {
+        req: {headers: {cookie: req.headers.get("cookie")}}
+      }
+    )
+
+  if (sesssion) return NextResponse.next();
+
+  return NextResponse
+          .redirect(new URL(SIGNIN, req.nextUrl.origin))
+}
