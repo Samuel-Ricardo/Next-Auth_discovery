4afd82e89e04d0054c31fe268e892b07755cf589
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-22 09:33:35 -0300

    :sparkles: update: page - Home (pages)

--- next-auth-demo/src/pages/index.tsx
+++ next-auth-demo/src/pages/index.tsx
@@ -1,11 +1,16 @@
+import { signOut, useSession } from 'next-auth/react'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const {data:session} = useSession();
+
   return (
-    <main className={`${inter.className} `}>
-      
+    <main className={`${inter.className} px-0 py-2`}>
+      <h1>{`Seja bem vindo :D ${session?.user?.name}`}</h1>
+      <button onClick={() => signOut()}>Sair >:()</button>
     </main>
   )
 }
